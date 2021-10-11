908ab7ba9f9aa32da22aff619a18a7287cc9ac22
Christofer Yeremia <christofer.yeremia@fifgroup.astra.co.id>
2021-10-11 14:57:21 +0700

    Project initialization

---
+++ src/main/java/id/co/astra/fifgroup/project/restfmunitidentification/RestfmUnitIdentificationApplication.java
@@ -0,0 +1,13 @@
+package id.co.astra.fifgroup.project.restfmunitidentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfmUnitIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfmUnitIdentificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/astra/fifgroup/project/restfmunitidentification/RestfmUnitIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.astra.fifgroup.project.restfmunitidentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfmUnitIdentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
